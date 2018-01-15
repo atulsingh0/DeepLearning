1fb5ab7826267934b6bad7327d9ab0dd26d0a8dd
Atul Singh <victimizeit@gmail.com>
2018-01-15 11:36:21 +0530

    c

---
+++ fast.ai/meLearning/utils.py
@@ -0,0 +1,51 @@
+# import
+import numpy as np
+import pandas as pd
+import os
+
+def get_sample(df, n):
+    '''
+        df: Inp - Pass the dataframe df
+        n : Inp - Fraction of data
+        df_sample : Out - dataframe with n% of data df
+    '''
+    df_sample = df.sample(frac=n)
+    return df_sample
+
+
+def df_summary(df):
+    print("Summary :")
+    return (df.info())
+
+    
+def df_null(df):
+    """Null Check in DataFrame"""
+    return df.isnull().sum()
+
+
+def df_null_per(df):
+    """Null Percentage Check in DataFrame"""
+    return df.isnull().sum().sort_index()/len(df)
+
+
+def rem_col_null(df, p):
+    '''
+    Remove the Columns which is having more than
+    p% NUll in it
+    
+    df: Inp - Input DataFrame
+    p : Inp - Percentage, , 0.1 = 10%
+    '''   
+    #return df[df.columns[list(df.isnull().sum()/df.size < p )]]
+    return df.loc[:, df.isnull().sum()/len(df) < p ]
+
+
+def rem_row_null(df, p):
+    '''
+    Remove the Row which is having more than
+    p% NUll in it
+    
+    df: Inp - Input DataFrame
+    p : Inp - Percentage, 0.1 = 10%
+    '''
+    return df.loc[(df.isnull().transpose().sum()/len(df) < p ).index]
