49b2a531e0a2d71a15a923adf67efb4c78b59c2c
atulsingh0 <victimizeit@gmail.com>
2018-03-27 22:55:40 +0530

    commit

--- MLaaS/run_ML_server.py
+++ MLaaS/run_ML_server.py
@@ -1,22 +1,21 @@
 from sklearn.externals import joblib
-from flask import Flask, abort, request, jsonify, Response
+from flask import Flask,  request,
 from flask_cors import CORS, cross_origin
 import numpy as np
-import json
 import re
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/iris', methods=['POST','GET'])
-#@cross_origin()
+@cross_origin()
 def predict_species():
     # Request
     # curl -d 'param="4.9,1,4,  0.5"' -X POST "http://0.0.0.0:5000/iris"
     clf = joblib.load('iris.model')
     req = re.sub('"', '', request.values['param'])
     q = np.array(req.split(','), dtype=np.float32).reshape(1, -1)
-    print(q)
+    #print(q)
 
     color_code = dict({0: 'Iris-Setosa', 1: 'Iris-Versicolour', 2: 'Iris-Virginica'})
     colors_pred = str([color_code[x] for x in clf.predict(q)])
@@ -24,4 +23,4 @@ def predict_species():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
